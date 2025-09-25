ef9fcc0fb735e58264a76312ebbae39dba740e7e
Javier <ibrahimcruz06@gmail.com>
2025-09-24 19:56:44 -0600

    cook functions creation on Pizza and Burger classes

--- tarea_3_Factory_Async/src/order.py
+++ tarea_3_Factory_Async/src/order.py
@@ -1,10 +1,13 @@
 class Order:
 
+    # Builder for Order class
     def __init__(self, name, order_id):
         self.name = name
         self.order_id = order_id
-
-
+    
+    # Prepare ordered food
+    def cook(self):
+        pass
 
 
         
--- tarea_3_Factory_Async/src/pizza.py
+++ tarea_3_Factory_Async/src/pizza.py
@@ -5,3 +5,7 @@ class Pizza(Order):
     def __init__(self, name, order_id):
         super().__init__(name, order_id)
 
+    def cook(self):
+        return f"Preparing order {self.order_id} (Pizza)"
+    
+    
